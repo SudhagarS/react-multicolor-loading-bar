24bdb4f203c030a7a6af6a197c97e11c61904697
Sudhagar <sudhagar.s@cleartax.in>
2020-04-26 20:50:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# react-colored-loading-bar
+
+Here is the [Demo](http://charts-demo.argick.com.s3-website.ap-south-1.amazonaws.com/).
+
+## Installation
+
+```bash
+npm install --save react-colored-loading-bar
+```
+
+## Example Usage
+
+```jsx
+import React from 'react'
+import Loadingbar from 'react-colored-loading-bar'
+
+const App = () => {
+  return (
+    <div>
+      <Loadingbar
+        colors={[darkBlue, darkOrange, darkBlue, lightOrange]}
+        height={5}
+        cycleDurationInMs={200}
+        positionAtTop={true}
+      ></Loadingbar>
+    </div>
+  )
+}
+
+export default App
+```
+
+## Props
+
+Below are the props you can pass to the Component to customize its behaviour.
+
+| Prop                                | Type            | Default          | description |
+| ----------------------------------- | --------------- | ---------------- | ----------- |
+| colors                              | string[]        | -                | Colors codes to fill the loading bar with |
+| height                              | number          | -                | Height of the bar |
+| cycleDurationInMs                   | number          | -                | How soon you want to the bar to complete one cycle |
+| positionAtTop                       | boolean         | -                | Whether to stick the bar at the top of the page |
+
+
+## License
+
+[ISC licensed](./LICENSE)
---
+++ src/loadingBar.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+
+type Maybe<T> = T | null;
+
+export type ProgressBarProps = {
+  colors: string[];
+  cycleDurationInMs: number;
+  height: number;
+  positionAtTop: boolean;
+};
+
+export type ProgressBarState = {
+  currentCycle: number;
+  currentTick: number;
+};
+
+export const rotateArray = (array: any[], times: number) => {
+  const rotatedArray = [...array];
+  while (times--) {
+    var temp = rotatedArray.pop();
+    rotatedArray.unshift(temp);
+  }
+  return rotatedArray;
+};
+
+export default class ProgressBar extends React.Component<ProgressBarProps, ProgressBarState> {
+  intervalId: Maybe<NodeJS.Timeout> = null;
+  bar: Maybe<HTMLElement> = null;
+  tickDurationInMs = 4;
+
+  constructor(props: ProgressBarProps) {
+    super(props);
+
+    this.rotateColors = this.rotateColors.bind(this);
+    this.getBoxes = this.getBoxes.bind(this);
+    this.getBarWidth = this.getBarWidth.bind(this);
+    this.getBoxesCount = this.getBoxesCount.bind(this);
+    this.getPixelsToMovePerTick = this.getPixelsToMovePerTick.bind(this);
+    this.getColorsCount = this.getColorsCount.bind(this);
+
+    this.state = {
+      currentCycle: 0,
+      currentTick: 0,
+    };
+  }
+
+  componentDidMount() {
+    const self = this;
+    const ticksPerCycle = this.props.cycleDurationInMs / 4;
+
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.intervalId = setInterval(() => {
+      self.setState({
+        currentTick: this.state.currentTick + 1,
+      });
+      if (this.state.currentTick + 1 === ticksPerCycle) {
+        self.setState({
+          currentTick: 0,
+          currentCycle: this.state.currentCycle + 1,
+        });
+      }
+    }, this.tickDurationInMs);
+  }
+
+  componentWillUnmount() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  getColorsCount() {
+    return this.props.colors.length;
+  }
+
+  getBoxesCount() {
+    return this.getColorsCount() + 1;
+  }
+
+  getBarWidth() {
+    return this.bar?.offsetWidth ?? 0;
+  }
+
+  getPixelsToMovePerTick() {
+    return this.getBarWidth() / this.props.cycleDurationInMs;
+  }
+
+  getBoxMaxWidth() {
+    return this.getBarWidth() / this.getColorsCount();
+  }
+
+  rotateColors = () => {
+    return rotateArray(this.props.colors, this.state.currentCycle % this.getColorsCount());
+  };
+
+  getBoxes() {
+    if (!this.getBarWidth()) {
+      return [];
+    }
+
+    let rotatedColors = this.rotateColors();
+    let boxesCount = this.getBoxesCount();
+    let boxes = [];
+    for (let i = 0; i < boxesCount; i++) {
+      let backgroundColor = i === 0 ? rotatedColors[rotatedColors.length - 1] : rotatedColors[i - 1];
+
+      let width;
+      let newWidthOffset = this.state.currentTick * this.getPixelsToMovePerTick();
+      if (i == 0) {
+        width = newWidthOffset;
+      } else if (i === boxesCount - 1) {
+        width = this.getBoxMaxWidth() - newWidthOffset;
+      } else {
+        width = this.getBoxMaxWidth();
+      }
+      boxes.push(<div key={i} style={{ height: this.props.height, backgroundColor, width }} />);
+    }
+    return boxes;
+  }
+
+  render() {
+    let positionStyles = {};
+    if (this.props.positionAtTop) {
+      positionStyles = {
+        position: 'absolute',
+        width: '100%',
+        top: 0,
+        left: 0,
+        right: 0,
+      };
+    }
+    return (
+      <div
+        style={{ height: this.props.height, display: 'flex', flexDirection: 'row', ...positionStyles }}
+        ref={(bar) => {
+          this.bar = bar;
+        }}
+      >
+        {this.getBoxes()}
+      </div>
+    );
+  }
+}
